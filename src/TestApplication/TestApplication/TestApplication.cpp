68fdc65d4972974f7b751752e46eeab0e06bb286
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2021-09-10 09:31:27 +0200

    Test Application change

--- src/TestApplication/TestApplication/TestApplication.cpp
+++ src/TestApplication/TestApplication/TestApplication.cpp
@@ -1,5 +1,6 @@
 ///////////////////////////////////////////////////////////////////
-//: Title        : CContextMenu.cpp
+//: Title        : TestApplication.cpp
+//: Product:     : Cpp.Include.Windows
 //: Date         : 2020-05-01
 //: Author       : "Kjetil Kristoffer Solberg" <post@ikjetil.no>
 //: Version      : 1.0.0.0
