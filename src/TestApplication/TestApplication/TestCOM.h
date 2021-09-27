359e92eaf6c70ea9c19521bf3604fc8d5562bc11
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2021-09-27 13:48:33 +0200

    Test commit

--- src/TestApplication/TestApplication/TestCOM.h
+++ src/TestApplication/TestApplication/TestCOM.h
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////
+//: Title        : TestCOM.h
+//: Product:     : Cpp.Include.Windows
+//: Date         : 2020-05-01
+//: Author       : "Kjetil Kristoffer Solberg" <post@ikjetil.no>
+//: Version      : 1.0.0.0
+//: Descriptions : Implementation of COM part of Cpp.Include.Windows Test Application.
 #pragma once
 //
 // #include
